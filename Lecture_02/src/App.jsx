c99eb6ebd15d9201d993343e81fac0c916489750
Nishant Paliwal <paliwalnishant2003@gmail.com>
2024-06-15 12:32:43 +0530

    Lecture_02 Login Form

---
+++ Lecture_02/src/App.jsx
@@ -0,0 +1,14 @@
+import Login from './Login';
+import Register from './Register';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login />
+      {/* <Register /> */}
+    </main>
+  );
+}
+
+export default App;
